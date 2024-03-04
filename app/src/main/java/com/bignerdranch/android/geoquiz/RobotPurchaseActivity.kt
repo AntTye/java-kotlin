5a678c62fe06ccd1556d014debb1af50ece58370
AntTye <w21.tyler@gmail.com>
2024-03-04 00:03:54 -0500

    Semi-finished-app, carry over energy needed for specific robots and not reset energy.

--- app/src/main/java/com/bignerdranch/android/geoquiz/RobotPurchaseActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/RobotPurchaseActivity.kt
@@ -1,4 +1,5 @@
 package com.bignerdranch.android.geoquiz
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -24,16 +25,15 @@ class RobotPurchaseActivity : ComponentActivity() {
     private lateinit var reward_g: TextView
     private lateinit var random_reward: TextView
 
-    private var robotTurn: Int = 0
-    private var energyValue: Int = 0
+    private var robot_turn_main: Int = 0
+    private var energy_amount_main: Int = 0
     private val purchasedRewards = mutableListOf<Int>()
-    private var lastPurchase: String = ""
 
     companion object {
-        fun newIntent(packageContext: Context, robotEnergy: Int, robotTurn: Int): Intent {
+        fun newIntent(packageContext: Context, robotEnergy: Int, robot_turn_main: Int): Intent {
             return Intent(packageContext, RobotPurchaseActivity::class.java).apply {
                 putExtra(EXTRA_ROBOT_ENERGY, robotEnergy)
-                putExtra(EXTRA_ROBOT_TURN, robotTurn)
+                putExtra(EXTRA_ROBOT_TURN, robot_turn_main)
             }
         }
     }
@@ -52,48 +52,86 @@ class RobotPurchaseActivity : ComponentActivity() {
         reward_g = findViewById(R.id.reward_g)
         random_reward = findViewById(R.id.random_reward)
 
-        robotTurn = intent.getIntExtra(EXTRA_ROBOT_TURN, 0)
-        energyValue = intent.getIntExtra(EXTRA_ROBOT_ENERGY, 0)
+        robot_turn_main = intent.getIntExtra(EXTRA_ROBOT_TURN, 0)
+        energy_amount_main = intent.getIntExtra(EXTRA_ROBOT_ENERGY, 0)
 
         setRobotTurn()
 
-        reward_a.setOnClickListener { makePurchase(1) }
-        reward_b.setOnClickListener { makePurchase(2) }
-        reward_c.setOnClickListener { makePurchase(3) }
-        reward_d.setOnClickListener { makePurchase(3) }
-        reward_e.setOnClickListener { makePurchase(4) }
-        reward_f.setOnClickListener { makePurchase(4) }
-        reward_g.setOnClickListener { makePurchase(7) }
-
+        reward_a.setOnClickListener     {
+            makePurchase(1)
+            val intent = MainActivity.newIntent(this, energy_amount_main)
+            startActivity(intent)
+        }
+        reward_b.setOnClickListener     {
+            makePurchase(2)
+            val intent = MainActivity.newIntent(this, energy_amount_main)
+            startActivity(intent)
+        }
+        reward_c.setOnClickListener     {
+            makePurchase(3)
+            val intent = MainActivity.newIntent(this, energy_amount_main)
+            startActivity(intent)
+        }
+        reward_d.setOnClickListener     {
+            makePurchase(3)
+            val intent = MainActivity.newIntent(this, energy_amount_main)
+            startActivity(intent)
+        }
+        reward_e.setOnClickListener     {
+            makePurchase(4)
+            val intent = MainActivity.newIntent(this, energy_amount_main)
+            startActivity(intent)
+        }
+        reward_f.setOnClickListener     {
+            makePurchase(4)
+            val intent = MainActivity.newIntent(this, energy_amount_main)
+            startActivity(intent)
+        }
+        reward_g.setOnClickListener     {
+            makePurchase(7)
+            val intent = MainActivity.newIntent(this, energy_amount_main)
+            startActivity(intent)
+        }
+        random_reward.setOnClickListener{
+            val randomCost = (1..7).random()
+            makePurchase(randomCost)
+            val intent = MainActivity.newIntent(this, energy_amount_main)
+            startActivity(intent)
+        }
     }
 
-        private fun makePurchase(costOfPurchase : Int){
-            val rewards = listOf(R.string.reward_a,R.string.reward_b,R.string.reward_c,R.string.reward_d,R.string.reward_f,R.string.reward_g,R.string.random_reward)
-            if(energyValue >= costOfPurchase){
-                val s1 = getString(rewards[costOfPurchase - 1])
+    private fun makePurchase(costOfPurchase : Int){
+        val rewards = listOf(R.string.reward_a,R.string.reward_b,R.string.reward_c,R.string.reward_d,R.string.reward_f,R.string.reward_g,R.string.random_reward)
+        if (energy_amount_main >= costOfPurchase) {
+            val indy = costOfPurchase - 1
+            if (purchasedRewards.contains(indy)) {
+                Toast.makeText(this, "You already purchased this reward", Toast.LENGTH_SHORT).show()
+            } else {
+                val s1 = getString(rewards[indy])
                 val s2 = "$s1 purchased"
-                energyValue -= costOfPurchase
-                energy_amount.setText(energyValue.toString())
+                energy_amount_main -= costOfPurchase
+                energy_amount.text = energy_amount_main.toString()
+                purchasedRewards.add(indy)
                 Toast.makeText(this, s2, Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(this, "Insufficient", Toast.LENGTH_SHORT).show()
             }
+        } else {
+            Toast.makeText(this, "Insufficient Energy", Toast.LENGTH_SHORT).show()
+            //finish()
         }
-
-
+    }
     private fun setRobotTurn() {
-        if (robotTurn == 1) {
+        if (robot_turn_main == 1) {
             energy_robot.setImageResource(R.drawable.robot_red_large)
-            energy_amount.text = energyValue.toString()
-        }else if (robotTurn == 2){
+            energy_amount.text = energy_amount_main.toString()
+        }else if (robot_turn_main == 2){
             energy_robot.setImageResource(R.drawable.robot_white_large)
-            energy_amount.text = energyValue.toString()
-        }else if (robotTurn == 3){
+            energy_amount.text = energy_amount_main.toString()
+        }else if (robot_turn_main == 3){
             energy_robot.setImageResource(R.drawable.robot_yellow_large)
-            energy_amount.text = energyValue.toString()
+            energy_amount.text = energy_amount_main.toString()
         } else {
             energy_robot.setImageResource(R.drawable.robot_red_large)
-            energy_amount.text = energyValue.toString()
+            energy_amount.text = energy_amount_main.toString()
         }
     }
 
