9eac1852c92691acd367da36fd17cc80096e36ae
AntTye <161770821+AntTye@users.noreply.github.com>
2024-03-06 21:20:24 -0500

    Mistake with merging, deleting.

--- Robots App/app/src/main/java/com/bignerdranch/android/geoquiz/Robot.kt
+++
@@ -1,8 +0,0 @@
-package com.bignerdranch.android.geoquiz
-
-// This data class holds values for our 'robots', red white yellow robot.
-data class Robot(
-    var myTurn      : Boolean,
-    val largeImgRes : Int,
-    val smallImgRes : Int,
-)
